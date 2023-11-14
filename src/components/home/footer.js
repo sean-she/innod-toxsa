ce19701cf23037ecfa45306945883410b4e9bab9
Clara Kim <02clara.kim@gmail.com>
2023-11-14 09:22:43 -0800

    footer finished

--- src/components/home/footer.js
+++ src/components/home/footer.js
@@ -5,38 +5,43 @@ import logo from "../../images/toxsa_logo.png"
 export default function Footer(props) {
 
     return (
-        <div className="footer-container">
-            <div className="logo-container">
-                <div className="contain-width">
-                    <img src={logo} alt="toxsa logo"/>
-
-                    <div className="logo-midsection">
-                        <div className="ocf">
-                            <a href="https://www.ocf.berkeley.edu">
-                                <img src="http://www.ocf.berkeley.edu/hosting-logos/ocf-hosted-penguin-dark.svg"
-                                alt="Hosted by the OCF"/>
-                            </a>
+        <div className="footers">
+            <hr id="topline"></hr>
+            <div className="footer-container">
+                <div className="logo-container">
+                    <div className="contain-width">
+                        <img src={logo} alt="toxsa logo"/>
+                        <div className="logo-midsection">
+                            <div id="leftspace"></div>
+                            <div className="ocf">
+                                <a id="ocflink" href="https://www.ocf.berkeley.edu">
+                                    <img src="http://www.ocf.berkeley.edu/hosting-logos/ocf-hosted-penguin-dark.svg"
+                                    alt="Hosted by the OCF"/>
+                                </a>
+                            </div>
+                            <div className="description">
+                                <p>We are a student group acting independently of the University of California. We take full responsibility for our organization and this web site.</p>
+                            </div>
+                            <div id="rightspace"></div>
                         </div>
-                        <div className="description">
-                            <p>We are a student group acting independently of the University of California. We take full responsibility for our organization and this web site.</p>
-                        </div>
+                        <p id="copyright">© 2023 Toxicology Student Association</p>
                     </div>
-                    <p id="copyright">© 2023 Toxicology Student Association</p>
                 </div>
-            </div>
-            <div className="footer-navigation">
-                <div className="footer-pages">
-                    <a href="#Home"><p>Home</p></a>
-                    <a href="#About-Us"><p>About Us</p></a>
-                    <a href="#Opportunities"><p>Opportunities</p></a>
-                    <a href="#Membership"><p>Become a Member</p></a>
-                </div>
-                <div className="footer-contact">
-                    <p>CONTACT US</p>
-                    <a href="#Instagram"><p>Instagram</p></a>
-                    <a href="mailto:berkeleytoxsa@gmail.com"><p>berkeleytoxsa@gmail.com</p></a>
+                <div className="footer-navigation">
+                    <div className="footer-pages">
+                        <a href="#Home"><p>Home</p></a>
+                        <a href="#About-Us"><p>About Us</p></a>
+                        <a href="#Opportunities"><p>Opportunities</p></a>
+                        <a href="#Membership"><p>Become a Member</p></a>
+                    </div>
+                    <div className="footer-contact">
+                        <p id="contactus">CONTACT US</p>
+                        <a href="#Instagram"><p>Instagram</p></a>
+                        <a href="mailto:berkeleytoxsa@gmail.com"><p>berkeleytoxsa@gmail.com</p></a>
+                    </div>
                 </div>
             </div>
         </div>
+        
     );
 }
