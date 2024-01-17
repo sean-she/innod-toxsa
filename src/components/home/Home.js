99d51a325490b4e8f200200d6d42b870b9dd04a8
sekar <sekarnareswari@berkeley.edu>
2024-01-17 12:24:05 -0800

    finished home hero

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "../Header/header";
 import Footer from "./footer";
 import Hero from ".//hero";
 import Body from "./body";
@@ -7,10 +6,7 @@ import Body from "./body";
 export default function Home() {
     return (
         <>
-            <Header />
-            <main>
-                <Hero />
-            </main>
+            <Hero />
 
             <Body />
 
--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import './hero.css'; // Import the CSS file
+import React from "react";
+import "./hero.css"; // Import the CSS file
+import Header from "../Header/header";
 import heroImage from "../../images/hexagon.png"; // Replace 'path_to_your_image.jpg' with the actual path to your image
 
 function Hero() {
     return (
-        <section className="hero-home">
-            <section className="hero">
-                <div className="hero-text">
-                    <h2 id="uc-berkeley">UC Berkeley</h2>
-                    <h1 className="club-name">Toxicology</h1>
-                    <h1 className="club-name">Student</h1>
-                    <h1 className="club-name">Association</h1>
-
-                </div>
-                <div className="hero-image">
-                    <img src={heroImage} />
-                </div>
-            </section>
+        <section className="home-hero">
+            <Header></Header>
+            <div className="home-hero-text">
+                <h2 id="uc-berkeley">UC Berkeley</h2>
+                <h1 className="club-name">Toxicology Student Association</h1>
+            </div>
+            <div className="hero-image">
+                <img src={heroImage} />
+            </div>
         </section>
-
-
-        
     );
 }
 
