3fb9055d6f316020088543967a659748a2902794
sekarnareswari <sekarnareswari2002@gmail.com>
2024-01-18 19:13:10 -0800

    finished home hero responsiveness

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./hero.css"; // Import the CSS file
 import Header from "../header/header";
 import heroImage from "../../images/home-hero.png";
+import heroImageMobile from "../../images/home-hero-mobile.png";
 
 function Hero() {
     return (
@@ -11,8 +12,11 @@ function Hero() {
                 <h2 id="uc-berkeley">UC Berkeley</h2>
                 <h1 className="club-name">Toxicology Student Association</h1>
             </div>
+            <div className="hero-image-mobile">
+                <img src={heroImageMobile}></img>
+            </div>
             <div className="hero-image">
-                <img src={heroImage} />
+                <img src={heroImage} alt="Toxicology Student Association" />
             </div>
         </section>
     );
